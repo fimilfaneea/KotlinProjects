0aab48060b560ae79ff6805cd75a96f6f458e2b0
Fimil Faneea <fimilfaneea@gmail.com>
2024-12-22 18:43:19 +0530

    first commit

--- app/src/main/java/com/example/calculator/Operations.kt
+++
@@ -1,8 +0,0 @@
-package com.example.calculator
-
-sealed class Operations(val symbol: String) {
-    object Add: Operations("+")
-    object Subtract: Operations("-")
-    object Multiply: Operations("x")
-    object Divide: Operations("/")
-}
--- app/src/main/java/com/example/calculator/State.kt
+++
@@ -1,8 +0,0 @@
-package com.example.calculator
-
-data class CalculatorState (
-    val number1: String ="",
-    val number2: String ="",
-    val operations: Operations? = null
-
-    )
